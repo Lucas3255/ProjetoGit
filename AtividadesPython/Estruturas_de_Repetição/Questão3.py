eec6dfd30ca2c03a026663dbea38c35f03c35edd
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2024-04-13 18:24:14 -0300

    Create Questão3.py

---
+++ AtividadesPython/Estruturas_de_Repetição/Questão3.py
@@ -0,0 +1,9 @@
+# 3 - Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa
+# para que ele mostre os números um ao lado do outro.
+
+print("Números de 1 a 20 abaixo do outro:")
+for i in range(1, 21):
+    print(i)
+print("Números de 1 a 20 ao lado do outro:")
+for i in range(1, 21):
+    print(i, end=" ")
