c676c124941053c844f08e265c448f86b831f83c
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2024-04-13 18:01:44 -0300

    Create Questão6.py

---
+++ AtividadesPython/Estrutura_Sequencial/Questão6.py
@@ -0,0 +1,5 @@
+# 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+r = float(input('Qual o raio do circulo? '))
+area = 3,14 * r ** 2
+print(f"A área do círculo é {area}")
