65980ff9eb46c8cd165977ebbc33138bafc0ead1
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2024-04-13 19:13:54 -0300

    Create Questão1.py

---
+++ AtividadesPython/Classes_e_Objetos1/Questão1.py
@@ -0,0 +1,24 @@
+# 1 - Crie uma classe que modele um quadrado:
+# Atributos: Tamanho do lado
+# Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+
+class Quadrado:
+    def __init__(self, lado):
+        self.lado = lado
+
+    def mudar_lado(self, novo_lado):
+        self.lado = novo_lado
+
+    def retornar_lado(self):
+        return self.lado
+
+    def calcular_area(self):
+        return self.lado ** 2
+
+quadrado1 = Quadrado(5)
+print(f"Lado do quadrado: {quadrado1.retornar_lado()}")
+print(f"Área do quadrado: {quadrado1.calcular_area()}")
+
+quadrado1.mudar_lado(7)
+print(f"Novo lado do quadrado: {quadrado1.retornar_lado()}")
+print(f"Nova área do quadrado: {quadrado1.calcular_area()}")
