643a5b2e33564ec551aa2db490a6c97dbd59dc3c
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2024-04-13 19:15:59 -0300

    Update Questão1.py

--- AtividadesPython/Classes_e_Objetos1/Questão1.py
+++ AtividadesPython/Classes_e_Objetos1/Questão1.py
@@ -1,6 +1,6 @@
 # 1 - Crie uma classe que modele um quadrado:
-# Atributos: Tamanho do lado
-# Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+# - Atributos: Tamanho do lado
+# - Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
 class Quadrado:
     def __init__(self, lado):
