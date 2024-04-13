ebcc1629fff34c265e99a30ec847b32c482078a3
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2024-04-13 17:59:49 -0300

    Create Questão3.py

---
+++ AtividadesPython/Estrutura_Sequencial/Questão3.py
@@ -0,0 +1,8 @@
+# 3 - Faça um Programa que peça dois números e imprima a soma.
+
+num1 = int (input("Escreva um número: "))
+num2 = int (input("Escreva outro número: "))
+
+total = num1 + num2
+
+print (f'A soma deles é: {total}')
